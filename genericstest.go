16aa91fff3d1a3c99708f5b4fbac716e6a490f17
johnson yuen <j@johnsonyuen.com>
2021-10-08 10:51:47 +1300

    Initial WIP

---
+++ genericstest.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func sha_sum(c *gin.Context) {
+	h := sha256.New()
+	h.Write([]byte("hello world\n"))
+	c.IndentedJSON(http.StatusOK, h.Sum(nil))
+}
+
+func headers(c *gin.Context) {
+
+	for name, headers := range c.{
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", sha_sum)
+	router.GET("/headers", headers)
+
+	router.Run("localhost:8090")
+}
